feb1cc1ee16e68f32f5dd52cb194ba223a3f079f
Kemirdin <qemirdin@gmail.com>
2017-12-21 19:59:58 +0100

    round compare

---
+++ week2/5-round-compare.js
@@ -0,0 +1,13 @@
+'use strict';
+
+//5-How do you round the number 7.25, to the nearest integer?
+
+let z = 7.25;
+console.log ("The Main Value is : ", z);
+let a = Math.round (7.25);
+console.log ("The Nearest Value Is : ", a);
+let c = Math.max (z, a);
+console.log ('the highest values is : ', c);
+
+
+
