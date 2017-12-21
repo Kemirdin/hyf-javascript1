6e71b4b17eaf3f6f9260c58d83800a584c261e9b
Kemirdin <qemirdin@gmail.com>
2017-12-21 18:11:33 +0100

    updated HelloWorld.js file

---
+++ week2/HelloWorld.js
@@ -0,0 +1,8 @@
+'use strict';
+//1-Write a console.log statement saying "Hello World!" for each language that you know.
+
+console.log("Hello, world!");
+console.log("Hallo,Wereld");
+console.log("你好，世界!");
+console.log("Merhaba, dünya!");
+console.log("こんにちは世界!");
