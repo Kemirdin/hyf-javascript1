32a09ba013677284df924597d4c3ee3af83fde52
Kemirdin <qemirdin@gmail.com>
2017-12-21 19:03:21 +0100

    saying hello world

--- week2/1-HelloWorld.js
+++ week2/1-HelloWorld.js
@@ -2,7 +2,7 @@
 //1-Write a console.log statement saying "Hello World!" for each language that you know.
 
 console.log("Hello, world!");
-console.log("Hallo,Wereld");
+console.log("Hallo,Wereld!");
 console.log("你好，世界!");
 console.log("Merhaba, dünya!");
 console.log("こんにちは世界!");
