eee42a91fa9180aa91dbd26ea7f29daab0b84d0f
Kemirdin <qemirdin@gmail.com>
2017-12-21 19:40:00 +0100

    declare a variable

---
+++ week2/4-String.js
@@ -0,0 +1,12 @@
+'use strict';
+
+//Declare a variable y and assign a string to it.
+
+let y = "I am a web developer";
+console.log ("the value of my string will be: I am a web developer");
+console.log (y);
+y = 'I try to hack myself';
+console.log ("the value of my string will be: I try to hack myself");
+
+console.log (y);
+
