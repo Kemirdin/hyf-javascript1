6217f883873129e883eeb3827f45c4fe4fc0efbb
Kemirdin <qemirdin@gmail.com>
2017-12-21 20:20:10 +0100

    declare an empty array

---
+++ week2/6-Arrays.js
@@ -0,0 +1,20 @@
+'use strict';
+
+//6. Arrays!
+//6.1 Declare an empty array. Make sure that the name you choose indicates 'plurality', because an array is capable of containing more than one element. (See Naming conventions.)
+//6.2 Write a console.log statement that explains in words what you think the value of the array is.
+// 6.3 Console.log your array.
+// 6.4 Create an array that has your favorite animals inside (see if you can find a good name that exactly describes what this variable will hold).
+// 6.5 Log your array.
+// 6.6 Add a statement that adds Daan's favorite animal ('baby pig') to the existing array.
+// 6.7 Log your new array!
+
+let plurality = [];
+console.log ("The value of array will be an empty square bracket:");
+console.log (plurality);
+
+let pets = ["dog", "cat", "horse"];
+console.log (pets);
+pets.push ("baby pig");
+console.log (pets);
+
