7a07b2cac2911555c097c46708e822e3a9308cf3
Kemirdin <qemirdin@gmail.com>
2017-12-21 20:33:02 +0100

    more strings let myString

---
+++ week2/7-MoreStrings.js
@@ -0,0 +1,10 @@
+// 7. More strings
+// 7.1 Let's consider the following string: let myString = "this is a test".
+// 7.2 Add the string to your file and console.log it.
+// 7.3 Find a way to get the length of myString.
+// 7.4 Console.log the length of myString.
+'use strict';
+
+let myString = " This is a test";
+console.log(" This is the my string :", myString);
+console.log(" The length of my string is :", myString.length);
