2068910ed721f42b77267e853038c7b0798f1a36
K3M1RD1N <33654086+Kemirdin@users.noreply.github.com>
2018-01-10 23:15:18 +0100

    Update main.js

--- week3/main.js
+++ week3/main.js
@@ -24,7 +24,7 @@ console.log(myString);
 //consider the following array:
 
 let favoriteAnimals = ['blowfish', 'capricorn', 'giraffe'];
-console.log(favoriteAnimals);
+
 //2.1 Add a statement that adds Mauro's favorite animal (turtle) to the existing array
 
 favoriteAnimals.push ('turtle');
@@ -32,14 +32,40 @@ favoriteAnimals.push ('turtle');
 //2.2 Log your new array!
 
 
-console.log(FavoriteAnimals);
+console.log(favoriteAnimals);
 //2.3 Now add Jim's favorite animal to the array, its a 'meerkat', but make sure it will be placed after 'blowfish' and before 'capricorn'.
+
+favoriteAnimals.splice(1, 0, 'meerkat');
 
 
 //2.4 Write a console.log statement that explains in words you think the new value of the array is.
+
+Console.log(favoriteAnimals);
 //2.5 Log your new new array!
+
+
 //2.6 Log the length of the array, add a message: "The array has a length of: "(here you should show the length of the array)
+
+console.log ('The array has a length of: ' + favoriteAnimals.length);
+
 //2.7 Jason does not like giraffes, delete this animal from the array
+
+favoriteAnimals.splice (3, 1);
+
+
+
 //2.8 Again log your new array.
+
+console.log(favoriteAnimals);
+
+
 //2.9 Now if unlike Jim, you don't like meerkats and you want to delete it from the array, but you don't know the position or the index of the item in the array, how can you find it?
+
+let indexOfMeerkat = favoriteAnimals.indexOf ('meerkat');
+
 //2.10 Log the index of meerkat to the console. Add a message so it says: "The item you are looking for is at index: " (here you should show the index of the item)
+
+console.log (
+  'The item you are looking for is at index: ' +
+    favoriteAnimals.indexOf ('meerkat')
+);
